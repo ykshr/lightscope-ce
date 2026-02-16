49d4d3e11018563f6e2f1c7fefe09c66ef102c41
Vibe Kanban <noreply@vibekanban.com>
2026-02-16 14:21:27 +0000

    chore: exclude shadcn components from CI/linting/formatting and revert changes

--- packages/web/eslint.config.js
+++ packages/web/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'src/__generated__/**/*'] },
+  { ignores: ['dist', 'src/__generated__/**/*', 'src/components/ui/**/*'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],

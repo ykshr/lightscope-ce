d1b57861666937618919966ebdfafec051dc1503
ykshr <ykshr02.23@gmail.com>
2026-03-11 04:22:58 +0000

    fix

--- packages/api/src/helpers/context.ts
+++ packages/api/src/helpers/context.ts
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-import { createClient } from '@clickhouse/client';
 
 dotenv.config();
 
@@ -9,13 +8,10 @@ dotenv.config();
 const CLICKHOUSE_HOST = process.env.CLICKHOUSE_HOST;
 const CLICKHOUSE_USERNAME = process.env.CLICKHOUSE_USERNAME;
 const CLICKHOUSE_PASSWORD = process.env.CLICKHOUSE_PASSWORD;
-const clickhouseClient = createClient({
-  url: CLICKHOUSE_HOST,
-  username: CLICKHOUSE_USERNAME,
-  password: CLICKHOUSE_PASSWORD,
-});
 const clickhouse = {
-  clickhouseClient,
+  CLICKHOUSE_HOST,
+  CLICKHOUSE_USERNAME,
+  CLICKHOUSE_PASSWORD,
 };
 
 export { clickhouse };

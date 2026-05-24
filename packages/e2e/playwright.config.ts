c60d77cc3cc4ad90936816a11e9a48368cb59392
ykshr <ykshr02.23@gmail.com>
2026-05-24 02:23:16 +0100

    fix

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -4,6 +4,7 @@ import { WEB_URL } from './tests/helpers/env';
 export default defineConfig({
   testDir: 'tests',
   globalSetup: 'tests/setup/global-setup.ts',
+  testIgnore: '**/*/*.unit.test.ts',
   use: {
     baseURL: WEB_URL,
     storageState: 'storage/auth.json',
